be6e04eb9aeafc23633a21c48b21973d8b588c75
Adam Falck <adam.falck@gmail.com>
2019-04-04 18:50:56 -0500

    301 take 2

--- server/main.js
+++ server/main.js
@@ -32,14 +32,27 @@ if(Meteor.isServer) {
     }
   });
 };
-WebApp.connectHandlers
-  .use('/redir/', function(req, res, next) {
-    // 301 Moved Permanently
+
+WebApp.connectHandlers.use(function(req, res, next) {
+
+  // Check if request is for non-www address
+  if (req.headers && req.headers.host.slice(0, 4) !== 'www.') {
+
+    // Add www. from host
+    var newHost = 'www.' + req.headers.host
+
+    // Redirect to www. version URL
     res.writeHead(301, {
-      'Location': 'https://www.isightrpv..com' + req.originalUrl
+      // Hard-coded protocol because req.protocol not available
+      Location: 'https://' + newHost + req.originalUrl
     });
     res.end();
-  });
+
+  } else {
+    // Continue with the application stack
+    next();
+  }
+});
 // Meteor.methods({
 // //facebook feed
 // FacebookFeed: function() {
