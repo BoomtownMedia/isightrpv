51e7ac340b3b2ff62f50a53e111d49d9601ad98d
Adam Falck <adam.falck@gmail.com>
2019-04-05 10:49:55 -0500

    web.com url forward isightrpv.com to wwwww.isightrpv.com

--- server/main.js
+++ server/main.js
@@ -1,28 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
 
-//301 redirect isightrpv.com to www.isightrpv.com
-WebApp.connectHandlers.use(function(req, res, next) {
-
-  // Check if request is for non-www address
-  if (req.headers && req.headers.host.slice(0, 4) !== 'www.') {
-
-    // Add www. from host
-    var newHost = 'www.' + req.headers.host
-
-    // Redirect to www. version URL
-    res.writeHead(301, {
-      // Hard-coded protocol because req.protocol not available
-      Location: 'http://' + newHost + req.originalUrl
-    });
-    res.end();
-
-  } else {
-    // Continue with the application stack
-    next();
-  }
-});
-
   // code to run on server at startup
   smtp = {
     username: 'boomtownm@gmail.com',
